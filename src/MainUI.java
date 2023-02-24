3d55b66c806a9b4c824124ea830c145a417df52a
joshuaobare <jobare.obare@gmail.com>
2023-02-24 19:23:08 +0300

    created right section of Main UI

--- src/MainUI.java
+++ src/MainUI.java
@@ -1,6 +1,9 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -17,10 +20,10 @@ public class MainUI extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Weather App");
         GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
+       // grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
-
+        grid.setPadding(new Insets(25, 25, 25, 25));
         Text weatherTitle = new Text("Overcast Clouds");
         Text location = new Text("Nairobi");
         Text date = new Text("Friday 24th Feb 2023");
@@ -31,12 +34,43 @@ public class MainUI extends Application {
         date.setFont(Font.font("Tahoma", FontWeight.NORMAL, 14));
         time.setFont(Font.font("Tahoma", FontWeight.NORMAL, 14));
         temperature.setFont(Font.font("Tahoma", FontWeight.NORMAL, 50));
-
         grid.add(weatherTitle , 0 ,0);
         grid.add(location , 0 ,1);
         grid.add(date , 0 ,2);
         grid.add(time , 0 ,3);
         grid.add(temperature , 0 ,4);
+
+        TextField locationSearchField = new TextField();
+        locationSearchField.setPromptText("Search location");
+        grid.add(locationSearchField, 0, 5);
+
+        Text feelsLike = new Text("Feels Like");
+        feelsLike.setFont(Font.font("Tahoma", FontWeight.NORMAL, 10));
+        Text feelsLikeValue = new Text("20C");
+        feelsLikeValue.setFont(Font.font("Tahoma", FontWeight.NORMAL, 32));
+        Text humidity = new Text("Humidity");
+        humidity.setFont(Font.font("Tahoma", FontWeight.NORMAL, 10));
+        Text humidityValue = new Text("69%");
+        humidityValue.setFont(Font.font("Tahoma", FontWeight.NORMAL, 32));
+        Text chanceOfRain = new Text("Chance of Rain");
+        chanceOfRain.setFont(Font.font("Tahoma", FontWeight.NORMAL, 10));
+        Text chanceOfRainValue = new Text("42%");
+        chanceOfRainValue.setFont(Font.font("Tahoma", FontWeight.NORMAL, 32));
+        Text windSpeed = new Text("Wind Speed");
+        windSpeed.setFont(Font.font("Tahoma", FontWeight.NORMAL, 10));
+        Text windSpeedValue = new Text("3.5M/S");
+        windSpeedValue.setFont(Font.font("Tahoma", FontWeight.NORMAL, 32));
+
+        grid.add(feelsLike , 7 ,0);
+        grid.add(feelsLikeValue , 7 ,1);
+        grid.add(humidity , 7 ,2);
+        grid.add(humidityValue , 7 ,3);
+        grid.add(chanceOfRain , 7 ,4);
+        grid.add(chanceOfRainValue , 7 ,5);
+        grid.add(windSpeed , 7 ,6);
+        grid.add(windSpeedValue , 7 ,7);
+
+
 
 
 
