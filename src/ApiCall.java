ff106254663b6a877f5bfbbdd3fd25800e2797df
joshuaobare <jobare.obare@gmail.com>
2023-02-25 21:32:28 +0300

    removed geocode method as the currentWeatherCall method will now retrieve location coordinate data

--- src/ApiCall.java
+++ src/ApiCall.java
@@ -20,73 +20,7 @@ public class ApiCall {
     public ApiCall(String location) {
         this.location = location;
         currentWeatherCall(this.location);
-        call(this.location);
-    }
-
-    // geocode takes in a location via user input and retrieves a set of coordinates via the openweathermap API
-    // the coordinates (lat and long) are used in the call() method
-    private void geocode(String location) {
-        try {
-            URL url = new URL(String.format("http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=1&appid=62d50fd38f48aba39355b8ae5a3ae053", location));
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-            int resCode = conn.getResponseCode();
-            if (resCode != 200) {
-                throw new RuntimeException("HttpResponseCode: " + resCode);
-            } else {
-                JSONParser parse = new JSONParser();
-                JSONArray dataArray = (JSONArray) parse.parse(new InputStreamReader(conn.getInputStream()));
-                JSONObject data = (JSONObject) dataArray.get(0);
-                //System.out.println(data);
-                locationData.put("country", (String) data.get("country"));
-                locationData.put("name", (String) data.get("name"));
-                locationData.put("lon", String.valueOf(data.get("lon")));
-                locationData.put("lat", String.valueOf(data.get("lat")));
-
-            }
-        } catch (Exception error) {
-            System.out.println(error);
-        }
-    }
-
-    private void call(String location) {
-        //geocode(location);
-
-        // a Gson object is initialized to convert JSON objects to HashMap objects
-        Gson gson = new Gson();
-        Type type = new TypeToken<HashMap<String, Object>>() {
-        }.getType();
-        try {
-            // a URL object is created based on the API endpoint
-            URL url = new URL(String.format("https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,precipitation_hours,windspeed_10m_max,winddirection_10m_dominant&current_weather=true&timezone=auto"
-                    , locationData.get("lat"), locationData.get("lon")));
-            System.out.println(url);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-
-            // we then connect to the API endpoint , the response code is collected
-            conn.connect();
-            int resCode = conn.getResponseCode();
-
-            // if the connection fails an error is thrown, else the data provided is parsed into a JSON object
-            if (resCode != 200) {
-                throw new RuntimeException("HttpResponseCode: " + resCode);
-            } else {
-
-                // Using the JSON simple library parse the string into a json object
-                JSONParser parse = new JSONParser();
-                JSONObject data_obj = (JSONObject) parse.parse(new InputStreamReader(conn.getInputStream()));
-
-                // the JSONObject is converted to a Hashmap object by the Gson object
-                weatherData = gson.fromJson(data_obj.toString(), type);
-
-                // from the weatherData the currentWeather is isolated
-                //currentWeather = (LinkedTreeMap<String, Object>) weatherData.get("current_weather");
-            }
-        } catch (Exception error) {
-            System.out.println(error);
-        }
+        weatherForecastCall(this.location);
     }
 
     public void currentWeatherCall(String location){
@@ -123,13 +57,49 @@ public class ApiCall {
                 locationData.put("lon" , lon);
                 locationData.put("lat" , lat);
                 locationData.put("locationName" , locationName);
-                System.out.println(locationName);
-                System.out.println(lat);
-                System.out.println(lon);
+
 
             }
         } catch(Exception e){
             System.out.println(e);
+        }
+    }
+
+    private void weatherForecastCall(String location) {
+
+        // a Gson object is initialized to convert JSON objects to HashMap objects
+        Gson gson = new Gson();
+        Type type = new TypeToken<HashMap<String, Object>>() {
+        }.getType();
+        try {
+            // a URL object is created based on the API endpoint
+            URL url = new URL(String.format("https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,precipitation_hours,windspeed_10m_max,winddirection_10m_dominant&current_weather=true&timezone=auto"
+                    , locationData.get("lat"), locationData.get("lon")));
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            // we then connect to the API endpoint , the response code is collected
+            conn.connect();
+            int resCode = conn.getResponseCode();
+
+            // if the connection fails an error is thrown, else the data provided is parsed into a JSON object
+            if (resCode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + resCode);
+            } else {
+
+                // Using the JSON simple library parse the string into a json object
+                JSONParser parse = new JSONParser();
+                JSONObject data_obj = (JSONObject) parse.parse(new InputStreamReader(conn.getInputStream()));
+
+                // the JSONObject is converted to a Hashmap object by the Gson object
+                weatherData = gson.fromJson(data_obj.toString(), type);
+
+                // from the weatherData the currentWeather is isolated
+                //currentWeather = (LinkedTreeMap<String, Object>) weatherData.get("current_weather");
+            }
+        } catch (Exception error) {
+            System.out.println(error);
         }
     }
 
