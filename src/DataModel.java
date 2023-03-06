a1ab2835dc16efd58488d7124714d6366f8e405a
joshuaobare <jobare.obare@gmail.com>
2023-03-06 18:38:37 +0300

    added userData class variable to DataModel

--- src/DataModel.java
+++ src/DataModel.java
@@ -6,6 +6,8 @@ public class DataModel {
         private HashMap<String, Object> currentWeather;
         private ArrayList<HashMap<String, Object>> weatherForecastData;
 
+        private HashMap<String,Object> userData;
+
         // the setData method is overloaded, with no parameter it calls the API with a default location
         public String setData(){
             ApiCall caller = new ApiCall("nairobi");
@@ -27,6 +29,14 @@ public class DataModel {
 
         public ArrayList<HashMap<String, Object>> getWeatherForecastData() {
             return weatherForecastData;
+        }
+
+        public HashMap<String,Object> getUserData(){
+            return userData;
+        }
+
+        public void setUserData(HashMap<String,Object> userData) {
+            this.userData = userData;
         }
 
 }
