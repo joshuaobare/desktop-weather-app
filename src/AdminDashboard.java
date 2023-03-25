69c2ff94bfe0121a265890284d5f487c4b7b5583
joshuaobare <jobare.obare@gmail.com>
2023-03-25 18:25:40 +0300

    moved User class back to its own file

--- src/AdminDashboard.java
+++ src/AdminDashboard.java
@@ -206,36 +206,3 @@ public class AdminDashboard extends Application {
     }
 }
 
-class User {
-    private final SimpleStringProperty name;
-    private final SimpleStringProperty dateAdded;
-
-    public User(SimpleStringProperty name, SimpleStringProperty dateAdded) {
-        this.name = name;
-        this.dateAdded = dateAdded;
-    }
-
-    public String getName() {
-        return name.get();
-    }
-
-    public SimpleStringProperty nameProperty() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name.set(name);
-    }
-
-    public String getDateAdded() {
-        return dateAdded.get();
-    }
-
-    public SimpleStringProperty dateAddedProperty() {
-        return dateAdded;
-    }
-
-    public void setDateAdded(String dateAdded) {
-        this.dateAdded.set(dateAdded);
-    }
-}
